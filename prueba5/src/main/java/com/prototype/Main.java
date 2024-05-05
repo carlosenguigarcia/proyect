e4ea87266ea5be50343e886dfb82db755e3d6682
carlosenguigarcia <66485636+carlosenguigarcia@users.noreply.github.com>
2024-05-05 21:59:44 +0200

    Paso 2

--- prueba5/src/main/java/com/prototype/Main.java
+++ prueba5/src/main/java/com/prototype/Main.java
@@ -2,6 +2,11 @@ package com.prototype;
 
 public class Main {
     public static void main(String[] args) {
+
+
         System.out.println("Hello world!");
+
+        System.out.println("How are you?");
+
     }
 }
