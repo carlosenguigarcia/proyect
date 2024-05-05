e691e9f8ea964ceb51f2bf7bddd5e3de3e6798da
carlosenguigarcia <66485636+carlosenguigarcia@users.noreply.github.com>
2024-05-05 21:57:18 +0200

    Paso

---
+++ Proyecto/src/main/java/com/example/ProyectoApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(ProyectoApplication.class, args);
+
+		System.out.println("Hola");
+	}
+
+}
